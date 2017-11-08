06b5d48fb54c4eb4c780af2e276cecd09ea8b394
naoangel <es.veneros@gmail.com>
2017-11-08 04:31:11 -0300

    z

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
-/*var boton = document.getElemenntById('btnstart');
-boton.addEventListener('click',function({
-})*/
+	/*var boton = document.getElemenntById('btnstart');
+	boton.addEventListener('click',function({
+	})*/
 
 
 
 
-var Tarea = document.getElementById('textbox')
+	var Tarea = document.getElementById('textbox')
 
-document.getElementById('textbox').value = '';
+	document.getElementById('textbox').value = '';
+	//limpiar el textarea
+		document.getElementById('textbox).value = '';
+
+		//contenedor que esta en el html
+		var cont = document.getElementById('cont');
+
+		//creamos el div que contiene cada comentario
+		var newComments = document.createElement('div');
+
+		//validar que textarea tenga un msje
+		if(comments.length == 0 || comments == null) {
+			alert('Debes ingresar un mensaje');
+			return false;
 
